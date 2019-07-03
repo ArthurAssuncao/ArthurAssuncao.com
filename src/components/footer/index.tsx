912356a889bb66447ec7cdf1be02c55e9763f132
Arthur Assuncao <arthur.xx92@gmail.com>
2019-07-02 23:44:11 -0300

    fix: footer components structure

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import Form from './form';
-import SocialButtons from './social';
-import ResumeButton from './resume';
+import Form from './Form';
+import SocialButtons from './Social';
+import ResumeButton from './Resume';
 
 const Foot = styled.footer`
   display: flex;
