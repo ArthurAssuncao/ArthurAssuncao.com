7630883c3ad9a37b15eeee7a295e429fca4b9812
Arthur Assuncao <arthur.xx92@gmail.com>
2019-07-02 23:39:50 -0300

    add: separate file for styled components of the header component

---
+++ src/components/header/index.style.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Head = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 50px;
+  background-color: ${props => props.theme.palette.primary[0]};
+  box-shadow: 0 0 5px 0 ${props => props.theme.palette.grayscale[1]};
+  position: fixed;
+  z-index: 11;
+  width: 100%;
+  height: ${props => props.theme.sizes.headerHeight};
+`;
+
+export default Head;
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
-import Nav from './nav';
-import styled from 'styled-components';
-
-const Head = styled.header`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0px 50px;
-  background-color: ${props => props.theme.palette.primary[0]};
-  box-shadow: 0 0 5px 0 ${props => props.theme.palette.grayscale[1]};
-  position: fixed;
-  z-index: 11;
-  width: 100%;
-  height: ${props => props.theme.sizes.headerHeight};
-`;
+import Nav from './Nav';
+import Head from './index.style';
 
 const Header = () => {
   return (
